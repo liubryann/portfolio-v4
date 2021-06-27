aae4c655c329d658823bcac60586cd6e91724ed9
Bryan Liu <liubryan.developer@gmail.com>
2021-06-27 17:11:06 -0400

    feat: added logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,6 +2,7 @@ import styles from './header.module.scss';
 import globalStyles from '../../styles/styles.module.scss';
 import Link from 'next/link';
 import Hamburger from '../Hamburger';
+import { useState } from 'react';
 
 export default function Header({ open, setOpen }) {
   const links = [
@@ -12,11 +13,20 @@ export default function Header({ open, setOpen }) {
     'contact'
   ]
 
+  const [logo, setLogo] = useState("/images/apollo_mad.png");
+
   return (
     <div>
     <header className={styles.headerWrapper}>
       <div>
-        <Link href="/"><a className={globalStyles.navStyle}>icon</a></Link>
+        <Link href="/">
+          <a 
+            onMouseEnter={() => setLogo("/images/apollo_happy.png")} 
+            onMouseLeave={() => setLogo("/images/apollo_mad.png")}
+          >
+            <img src={logo} className={styles.apollo}/>
+          </a>
+        </Link>
       </div>
       <ul className={`${styles.navWrapper} ${globalStyles.navStyle} ${globalStyles.hideMobile}`}>
         { links.map((link, index) => {
